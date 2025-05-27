8367b673fe5fe988bd73d15a6bcf68702bcf3c4b
WiSeokHwi <tjrgnl2115@gmail.com>
2025-05-27 17:04:11 +0900

    25.05.27.17.04

--- Assets/00.Scripts/PlayerScripts/PlayerController.cs
+++ Assets/00.Scripts/PlayerScripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,18 +10,25 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private PlayerInput input;
     
+    [ReadOnly]
+    [SerializeField]
+    private Vector3 velocity;
+    
     // 값 저장하는 변수
     private bool isGrounded;
-    private Vector3 velocity;
     private Vector2 moveVelocity;
     private bool isRun;
     private float speed; // 플레이어 이동속도
+
+    private float smoothTargetSpeed;
+    Vector2 smoothVelocity;
     
     // 플레이어 속도, 점프 등 수치
     [SerializeField] private float gravity = -9.81f; // CharacterController 중력 구현을 위한 중력값
     [SerializeField] private float workSpeed = 5f;
     [SerializeField] private float runSpeed = 10f;
-    [SerializeField] private float damping = 1f; // 속도 변화 부드럽게 하는 값
+    [SerializeField] private float damping = 0.25f; // 속도 변화 부드럽게 하는 값
+    [SerializeField] private float targetSpeedDamping = 0.8f;
     [SerializeField] private float jumpHeight = 3f; // 점프 힘
 
     private void Awake()
@@ -50,18 +58,33 @@ public class PlayerController : MonoBehaviour
     void Velocity()
     {
         YVelocity();
-        speed = isRun ? runSpeed : workSpeed;
-        moveVelocity = Vector2.Lerp(moveVelocity, input.inputMove, damping * Time.deltaTime);
-        velocity = new (moveVelocity.x, velocity.y, moveVelocity.y);
+        float targetSpeed = isRun ? runSpeed : workSpeed;
+        
+        speed = Mathf.SmoothDamp(speed, targetSpeed, ref smoothTargetSpeed, targetSpeedDamping);
+
+        Vector2 targetVelocity = input.inputMove;
+        moveVelocity = Vector2.SmoothDamp(moveVelocity, targetVelocity, ref smoothVelocity, damping);
+        
+        velocity.x = moveVelocity.x;
+        velocity.z = moveVelocity.y;
+        
+        Debug.Log(moveVelocity.magnitude * speed);
+        
     }
 
     void YVelocity()
     {
-        if (isGrounded && velocity.y <= 0) // 땅에 닿아있고 y의 속도가 가 0보다 작으면 
+        if (isGrounded) // 땅에 닿아있고 y의 속도가 가 0보다 작으면 
         {
-            velocity.y = -2f;
+            if (velocity.y <= 0)
+            {
+                velocity.y = -2f;
+            }
+            
         }
-        
-        velocity.y += gravity * Time.deltaTime;
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
     }
 }
